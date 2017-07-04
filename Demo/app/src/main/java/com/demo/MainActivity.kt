b5caa820b5f19a441c6c21f421bdc3e93fd45f5c
yr254gitLab <yr254@mail.yonrun.com>
2017-07-04 10:57:06 +0800

    delete support library,reduce app size

--- Demo/app/src/main/java/com/demo/MainActivity.kt
+++ Demo/app/src/main/java/com/demo/MainActivity.kt
@@ -1,81 +1,40 @@
 package com.demo
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
 import android.widget.Toast
-import com.hls.kotlin.HLSBaseItem
-import com.hls.kotlin.HLSManager
-import com.hls.kotlin.ObserverManager
-import com.hls.kotlin.SampleObserver
+import com.hls.kotlin.*
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
+    var status = DownloadStatus.NONE
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         HLSManager.init(this)
+
         start.setOnClickListener {
             var url = edit_text.text
             if (TextUtils.isEmpty(url)) {
                 Toast.makeText(baseContext, "url is null", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            var observer = ObserverManager.getExistObserver(url.toString())
-            if (observer == null) {
-                observer = object : SampleObserver() {
 
-                    override fun update(downloader: HLSBaseItem) {
-                        super.update(downloader)
-                        runOnUiThread {
-                            progressBar.progress = downloader.getProgress().toInt()
-                            bar_text.text = "${downloader.getProgress()}%"
-                        }
-                    }
-
-                }
-                ObserverManager.addNewObserver(url.toString(), observer)
+            if (status == DownloadStatus.NONE || status == DownloadStatus.PENDING || status == DownloadStatus.PAUSED || status == DownloadStatus.ERROR) {
+                startLoad(url.toString())
+            } else if (status == DownloadStatus.DOWNLOADING) {
+                pauseLoad(url.toString())
             }
-            HLSManager.startOrResumeDownloader(url.toString(), observer)
-        }
 
-        pause.setOnClickListener {
-            var url = edit_text.text
-            if (TextUtils.isEmpty(url)) {
-                Toast.makeText(baseContext, "url is null", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            HLSManager.pauseDownloader(url.toString())
-        }
-
-        resume.setOnClickListener {
-            var url = edit_text.text
-            if (TextUtils.isEmpty(url)) {
-                Toast.makeText(baseContext, "url is null", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            var observer = ObserverManager.getExistObserver(url.toString())
-            if (observer == null) {
-                observer = object : SampleObserver() {
-
-                    override fun update(downloader: HLSBaseItem) {
-                        super.update(downloader)
-                        runOnUiThread {
-                            progressBar.progress = downloader.getProgress().toInt()
-                            bar_text.text = "${downloader.getProgress()}%"
-                        }
-                    }
-
-                }
-                ObserverManager.addNewObserver(url.toString(), observer as SampleObserver)
-            }
-            HLSManager.startOrResumeDownloader(url.toString(), observer!!)
         }
 
         delete.setOnClickListener {
@@ -87,6 +46,8 @@ class MainActivity : AppCompatActivity() {
             doAsync {
                 HLSManager.deleteDownloader(url.toString())
                 uiThread {
+                    start.text = "开始"
+                    status = DownloadStatus.NONE
                     progressBar.progress = 0
                     bar_text.text = "0%"
                     Toast.makeText(baseContext, "file has be deleted", Toast.LENGTH_SHORT).show()
@@ -94,6 +55,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
         play.setOnClickListener {
             var url = edit_text.text
             if (TextUtils.isEmpty(url)) {
@@ -106,5 +68,44 @@ class MainActivity : AppCompatActivity() {
             intent.setDataAndTypeAndNormalize(uri, type)
             startActivity(intent)
         }
+    }
+
+
+    fun startLoad(url: String) {
+        var observer = ObserverManager.getExistObserver(url)
+        if (observer == null) {
+            observer = object : SampleObserver() {
+
+                override fun update(downloader: HLSBaseItem) {
+                    super.update(downloader)
+                    if (downloader.downloadStatus == DownloadStatus.DOWNLOADING) {
+                        runOnUiThread {
+                            start.text = "暂停"
+                            status = downloader.downloadStatus
+                            progressBar.progress = downloader.getProgress().toInt()
+                            bar_text.text = "${downloader.getProgress()}%"
+                        }
+                    } else if (downloader.downloadStatus == DownloadStatus.ERROR) {
+                        runOnUiThread {
+                            start.text = "失败"
+                            status = downloader.downloadStatus
+                            progressBar.progress = downloader.getProgress().toInt()
+                            bar_text.text = "${downloader.getProgress()}%"
+                            Toast.makeText(baseContext, "${downloader.fail_reason}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+
+            }
+            ObserverManager.addNewObserver(url, observer)
+        }
+        HLSManager.startOrResumeDownloader(url, observer)
+    }
+
+    fun pauseLoad(url: String) {
+        HLSManager.pauseDownloader(url)
+        start.text = "继续"
+        status = DownloadStatus.PAUSED
     }
 }
